3586f36a35128b3e3ef9f7b2d726200c9efccd7b
Michelly <michellykaren15@gmail.com>
2024-02-29 21:00:14 -0300

    stack with structs

--- src/stack.c
+++ src/stack.c
@@ -2,71 +2,97 @@
 #include <stdlib.h>
 #define SIZE 10
 
-void push();
-void pop();
-void display();
+//create stack struct 
+typedef struct {
+    int stack[SIZE];
+    int top;
+} Stack;
 
-int top = -1;       //value of top is initialised to -1
-                    //global variable to be changed 
-int stack[SIZE];    //array will implement the stack
-                    //global variable to be changed
-int main() {
-    printf("Olá, mundo!\n");
-    printf("Minha stack em C\n");
-    int choise;
-    while(1){
-        printf("Choises\n");
-        printf("1. Push\n");
-        printf("2. Pop\n");
-        printf("3. Display\n");
-        printf("4. Quit\n");
-        scanf("%d", &choise);
+void init(Stack *s){
+    for (int i = 0; i < SIZE; i++) {
+        s->stack[i] = 0;
+    }
+    s->top = -1;
+}
 
-        switch (choise)
-        {
-        case 1:
-            push();
-            break;
-        case 2:
-            pop();
-            break;
-        case 3:
-            display();
-            break;
-        case 4:
-            exit(0);    //exit() termina imediatamente a execução do prograda. 
-                        //exit(1) é quando encontra um erro
-            break;
-        
-        default:
-            printf("Invalid choice\n");
-            break;
-        }
+int isFull(Stack *s){
+  if (s->top==SIZE-1){
+    printf("Overflow!");
+    return 1;
+  }
+  return 0;
+}
+
+int isEmpty(Stack *s){
+    if(s->top==-1){
+      printf("Empty stack\n");
+      return 1;
     }
     return 0;
 }
 
-void push(){
-    int item;   //value to be pushed
-    if (top == (SIZE-1)) printf("Stack overflow!\n"); //case overflow is into push
-    else{
-        printf("Enter the item to be PUSHED");
-        scanf("%d", &item);
-        top=top+1;             //remember that top is started to -1
-        stack[top]=item;    //pushing item
-    }
+void push(Stack *s){
+  int item;
+  if(isFull(s)){}
+  else{
+    printf("Enter the item to be PUSHED");
+    scanf("%d", &item);
+    s->top++;
+    s->stack[s->top] = item;
+  }
 }
 
-void pop(){
-    if (top == -1) printf("Empty stack\n");
-    else{
-        printf("The deleted item is: %d\n", stack[top]);
-        stack[top] = 0;
-        top=top-1;
-    }
+void pop(Stack *s){
+  if(isEmpty(s)){}
+  else{
+    printf("The deleted item is: %d\n", s->stack[s->top]);
+    s->stack[s->top] = 0;
+    s->top--;
+  }
 }
 
-void display(){
+int main() {
+  Stack s;
+  init(&s);
+
+  printf("Olá, mundo!\n");
+  printf("Minha stack em C\n");
+  int choise;
+  while(1){
+      printf("Choises\n");
+      printf("1. Push\n");
+      printf("2. Pop\n");
+      printf("3. Display\n");
+      printf("4. Quit\n");
+      scanf("%d", &choise);
+
+      switch (choise)
+      {
+      case 1:
+          push(&s);
+          break;
+      case 2:
+          pop(&s);
+          break;
+      case 3:
+          //display();
+          break;
+      case 4:
+          exit(0);    //exit() termina imediatamente a execução do prograda. 
+                      //exit(1) é quando encontra um erro
+          break;
+      
+      default:
+          printf("Invalid choice\n");
+          break;
+      }
+  }
+  return 0;
+}
+
+
+
+/*void display(){
     //walking through the stack
     printf("-------------------\n");
     printf("-------STACK-------\n");
@@ -78,4 +104,4 @@ void display(){
         else printf("\n");
     }
     printf("-------------------\n");
-}
+}*/
